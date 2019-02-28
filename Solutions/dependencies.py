61f6d9eb5b31da29eec619f3372056269cebd9a7
Praise25 <39573519+Praise25@users.noreply.github.com>
2019-02-28 20:35:43 +0100

    Updated Solutions

--- Solutions/dependencies.py
+++ Solutions/dependencies.py
@@ -1,20 +1,44 @@
+photo_objs = []
+
 class Slides:
     def __init__(self):
         self.photos = []
+        self.tags = []
 
     def add_photo(self, photo):
         self.photos.append(photo)
+        for tag in photo.tags:
+            if tag in self.tags:
+                continue
+            else:
+                self.tags.extend(photo.tags)
+
+
+
+
 
 class Photo:
     _id = 0
-    def __init__(self, orient, tag_num, *tags):
+    def __init__(self, orient, tag_num, tags):
         self.orientation = orient
         self.tag_num = tag_num
-        self.tags = list(tags)
+        self.tags = tags
         self.id = self._id
         self._id += 1
 
     def __repr__(self):
         return "Photo {0.id}:,  Orientation: {0.orientation}, Tag Number: {0.tag_num}, Tags: {0.tags}".format(self)
+
+def parser(filename):
+    with open(filename, "r") as file:
+        photo_num = file.readline()
+        for line in file:
+            photo_details = line.strip("\n").split(" ")
+            orientation = photo_details[0]
+            tag_num = photo_details[1]
+            tags = photo_details[2:]
+            photo = Photo(orientation, tag_num, tags)
+            photo_objs.append(photo)
+
 
 
--- Solutions/solution.py
+++ Solutions/solution.py
@@ -1,8 +1,1 @@
 from dependencies import Photo, Slides
-
-photo1 = Photo("V", "2", "cat", "beach")
-print(photo1)
-
-slide1 = Slides()
-slide1.add_photo(photo1)
-print(slide1.photos)
